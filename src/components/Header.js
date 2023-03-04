b1380222e9789602a9c43a4fd36d612517bc1e5b
Shubham Barwad <sbarwad20595@gmail.com>
2023-03-04 18:08:55 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='header flex-row align-items' id='header'>
+        <h1 className='header-ls'>beano.</h1>
+        <h1 className='header-ss'>b.</h1>
+        <div className='menu flex-row align-items'>
+            <span className="material-symbols-rounded">notifications</span>
+            <span className="material-symbols-rounded">chat</span>
+            <span className="material-symbols-rounded">help</span>
+            <span className="material-symbols-rounded">person</span>
+        </div>
+    </div>
+  )
+}
---
+++ src/components/UserInfo.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import profile from './resources/user_profile_pic.jpg'
+
+export default function UserInfo() {
+  return (
+    <div className='user-info flex-col'>
+      <div className='flex-row align-items profile-img-name'>
+        <img className='profile-pic' src={profile} alt="profilepic" />
+        <p className='user-info-name level-1-font-size'>Shubham Barwad</p>
+        <i className="material-symbols-rounded">person_add</i>
+      </div>
+      <div className='flex-col user-info-content uic-1'>
+        <div className='flex-row align-items'>
+          <i className="material-symbols-rounded">location_on</i>
+          <p className='level-2-font-size'>Navi Mumbai, India</p>
+        </div>
+        <div className='flex-row align-items'>
+          <i className="material-symbols-rounded">work</i>
+          <p className='level-2-font-size'>Full-Stack Web Developer</p>
+        </div>
+      </div>
+      <div className='flex-col user-info-content uic-2'>
+        <div className='flex-row align-items justify-content-between'>
+          <p className='level-2-font-size'>Who's viewed your profile</p>
+          <p><strong className='dark-font'>4561</strong></p>
+        </div>
+        <div className='flex-row align-items justify-content-between'>
+          <p className='level-2-font-size'>Impressions of your post</p>
+          <p><strong className='dark-font'>4561</strong></p>
+        </div>
+      </div>
+      <div className='flex-col user-info-content uic-3'>
+        <p className='level-1-font-size'><strong>Social Profiles</strong></p>
+        <div className='flex-row align-items justify-content-between'>
+          <div className="align-items flex-row uic-3-d">
+          <i className="fa-brands fa-twitter"></i>
+          <div className='flex-col'>
+            <p className='level-1-5-font-size'><strong>Twitter</strong></p>
+            <p className="level-2-font-size">Social Network</p>
+          </div>
+        </div>
+          <i class="material-symbols-rounded">edit</i>
+        </div>
+        <div className='flex-row align-items justify-content-between'>
+          <div className="align-items flex-row uic-3-d">
+          <i className="fa-brands fa-twitter"></i>
+          <div className='flex-col'>
+            <p className='level-1-5-font-size'><strong>Linkedin</strong></p>
+            <p className="level-2-font-size">Network Platform</p>
+          </div>
+        </div>
+          <i class="material-symbols-rounded">edit</i>
+        </div>
+      </div>
+    </div>
+  )
+}
+
